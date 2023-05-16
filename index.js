4280ce3ec9cfa69bfb7ea7eeaa62260e902cc4de
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-05-16 18:10:16 +0300

    Step 2 done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export default function solution(content) {
   // BEGIN
   // Data normalization
@@ -8,9 +10,13 @@ export default function solution(content) {
 
   const clearData = data.map((plant) => plant.filter((element) => element));
   const normalizedData = clearData.map((plant) => plant.map((element) => element.trim()));
-  console.log(normalizedData);
 
   // Step 1
   console.log(`Количество растений: ${normalizedData.length}`);
+
+  // Step 2
+  const plantList = normalizedData.map(item => `${item[0][0].toUpperCase()}${item[0].slice(1)}`).sort();
+
+  console.log(plantList);
   // END
 }
