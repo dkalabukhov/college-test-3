714d7afd984bfceb3b322f56477d596bf6a04cc7
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-05-16 19:07:54 +0300

    Step 3 done

--- index.js
+++ index.js
@@ -18,5 +18,17 @@ export default function solution(content) {
   const plantList = normalizedData.map(item => `${item[0][0].toUpperCase()}${item[0].slice(1)}`).sort();
 
   console.log(plantList);
+
+  // Step 3
+  const dangerousPlants = normalizedData.filter((item) => item[4] === 'Да');
+  const safePlants = normalizedData.filter((item) => item[4] === 'Нет');
+
+  const procentOfDangerousPlants = (dangerousPlants.length / normalizedData.length) * 100;
+  const procentOfSafePlants = (safePlants.length / normalizedData.length) * 100;
+
+  console.log(`
+  Процент безопасных для человека растений: ${procentOfSafePlants}%
+  Процент опасных для человека растений: ${procentOfDangerousPlants}%`);
+
   // END
 }
