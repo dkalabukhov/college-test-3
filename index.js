9d13e48c3920abb0929c35b12c9ce11beef181a1
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-05-16 17:23:23 +0300

    Step 1 done

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 export default function solution(content) {
   // BEGIN
-  console.log(content)
+  // Data normalization
+  const rows = content.split('\n');
+  const list = rows.slice(2);
+
+  const data = list.map((element) => element.split('|'));
+
+  const clearData = data.map((plant) => plant.filter((element) => element));
+  const normalizedData = clearData.map((plant) => plant.map((element) => element.trim()));
+  console.log(normalizedData);
+
+  // Step 1
+  console.log(`Количество растений: ${normalizedData.length}`);
   // END
 }
